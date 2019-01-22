970cb3881571b1e2beac7553c701a9a8a4590d6f
sara.vikstrom.edvinsson <saed0037@ad.umu.se>
2019-01-22 09:14:34 +0100

    Update Program.cs

--- generic_solution/Program.cs
+++ generic_solution/Program.cs
@@ -6,10 +6,49 @@ using System.Threading.Tasks;
 
 namespace generic_solution
 {
+    public class DataSample<T>
+    {
+        private T theData;
+
+        public DataSample(T theData)
+            {
+                this.theData = theData;
+            }
+
+        public T GetData()
+        {
+            return theData;
+        }
+
+        public String MyToString()
+        {
+            return theData.ToString();
+        }
+
+        public static T getMiddleElement(T[] arr)
+        {
+            T element = arr[arr.Length / 2];
+            return element;
+        }
+    }
+
     class Program
     {
+
         static void Main(string[] args)
         {
+
+            DataSample<String> dss = new DataSample<string>("Forty-two");
+            DataSample<int> dsi = new DataSample<int>(42);
+
+            String s = dss.GetData();
+            int i = dsi.GetData();
+
+            String[] words = { "C++", "MacOS", "C#", "Android", "Python" };
+            String middle = DataSample<String>.getMiddleElement(words);
+
+            Console.WriteLine(String.Format("s={0}, i={1}  and middle={2}", s, i, middle));
+            Console.ReadKey();
         }
     }
 }
